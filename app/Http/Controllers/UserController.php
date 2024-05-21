8c310e07ee5c87f879fcbb811d84cd6f244674bb
Ostolski Marcin <ostolski.marcin@gmail.com>
2024-05-21 20:22:48 +0200

    user controller
    user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use User;
 
 class UserController extends Controller
 {
-    //
+    public function index() : View{
+        $models = User::where('IsActive', true)->get();
+        return view("User.index", ["models" => $models]);
+    }
+
+    public function create() : View{
+        $model = new User();
+        $model -> created_at = date('Y-m-d'); 
+        $model -> updated_at = date('Y-m-d');
+        return view("User.create", ["model" => $model]);
+    }
+    public function edit(int $id) : View{
+        $model = User::find($id);
+        return view("User.edit", ["model" => $model]);
+    }
+    
+    public function addToDb(Request $request) : RedirectResponse {
+        $model = new User();
+        $model -> name = $request->input("Name"); 
+        $model -> email = $request->input("Email");
+        $model -> password = $request->input("Password");
+        $model -> created_at = date('Y-m-d');
+        $model -> updated_at = date('Y-m-d');
+        $model -> isActive = true;
+        $model -> save();
+        return redirect('/users'); 
+     }   
+     public function update(Request $request, int $id) : RedirectResponse {
+         $model = User::find($id);
+         $model -> name = $request->input("Name"); 
+         $model -> quantity = $request->input("Quantity");
+         $model -> unit = $request->input("Unit");
+         $model -> updated_at = date('Y-m-d');
+         $model -> save();
+         return redirect('/users'); 
+      }   
+ 
+      public function delete(Request $request, int $id) : RedirectResponse {
+         $model = User::find($id);
+         $model -> IsActive = false;
+         $model -> save();
+         return redirect('/users'); 
+      }   
 }
