0f362471ef1b4e0b30a41a8663a57a9a97fa3bad
Paulina Ostolska <pa.ostolska@gmail.com>
2024-05-21 19:29:28 +0200

    changed css and js location
    ..

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>@yield("title")</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+<link rel="stylesheet" href="/css/bootstrap.min.css" />
     <link
         href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
         rel="stylesheet">
