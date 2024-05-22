4156741b96277fe4b0cd12ace3cf98816f5811ea
Ostolski Marcin <ostolski.marcin@gmail.com>
2024-05-22 22:05:16 +0200

    Squashed commit of the following:
    commit ce46735a020fe600235073bb629d59f0f9cdefca
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Wed May 22 22:03:19 2024 +0200
    
        22.05
    
    commit 81d9d2c4b2716c90bec44492d39a5a8e90fcd48d
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 22:23:54 2024 +0200
    
        foreignKey in Recipes view
    
    commit 94c77e74ac8acb061c00fb05b040c9d7e32777bc
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 22:14:16 2024 +0200
    
        foreingKeys in views
    
        foreingKeys in views
    
    commit fb3c5e8b1f7522994ab488f1f43e4d75577b177a
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:29:33 2024 +0200
    
        Squashed commit of the following:
    
        commit dbe8f6eaf83f10a43f06a431a8a8cfbb1eb6d057
        Merge: 17e06a2 bda033e
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 21:22:40 2024 +0200
    
            Merge branch 'paula'
    
        commit bda033ecf0c7b3cbdf1621f84e73b37fb2760f8e
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 21:22:21 2024 +0200
    
            zmiana buttonow
    
        commit 17e06a2831846bb6cf4cfdaa05ca7a995099681f
        Merge: 1845a84 466204e
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 20:21:02 2024 +0200
    
            Merge branch 'paula'
    
        commit 466204eddd728a02c2cbd21d07a5b6249b2f20c9
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 20:20:45 2024 +0200
    
            categories
    
    commit bbb1315e7465bb7e3d375e736165c0cd5dc97790
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:28:12 2024 +0200
    
        namespacesFixed
    
    commit 11355a3c0b6e9bbfbe4f01b01a267c0704736552
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:26:45 2024 +0200
    
        namespacesFixed
    
    commit 3826b9374845d501b7b6036b9c8c65289f0c1280
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:25:45 2024 +0200
    
        userController
    
    commit ef0ef98ec2d31384ec46665de8aab8ce6e9fe778
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:19:29 2024 +0200
    
        userController
    
    commit 68585dfde36b00313bbf85b29e2ac2c8b07f9c78
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 21:18:08 2024 +0200
    
        userController
    
    commit 158301e61e365bfa4116002a88c393a86a856080
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 20:23:13 2024 +0200
    
        Squashed commit of the following:
    
        commit 17e06a2831846bb6cf4cfdaa05ca7a995099681f
        Merge: 1845a84 466204e
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 20:21:02 2024 +0200
    
            Merge branch 'paula'
    
        commit 466204eddd728a02c2cbd21d07a5b6249b2f20c9
        Author: Paulina Ostolska <pa.ostolska@gmail.com>
        Date:   Tue May 21 20:20:45 2024 +0200
    
            categories
    
    commit 8c310e07ee5c87f879fcbb811d84cd6f244674bb
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 20:22:48 2024 +0200
    
        user controller
    
        user controller
    
    commit 27fb113525b8fe68d895d9027648b6e59d51a810
    Merge: f3e9a56 1845a84
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 19:38:19 2024 +0200
    
        Merge branch 'main' into marcin
    
    commit f3e9a56338b4d5559c46206f4bfcc16ab4af8665
    Merge: b284b1b 5f91080
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 19:29:23 2024 +0200
    
        Merge branch 'main' into marcin
    
    commit b284b1bb80f0f4b779cd3389472bf8f32ca77ef0
    Merge: e97b1e4 560d0ac
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 19:28:18 2024 +0200
    
        Merge branch 'main' into marcin
    
    commit e97b1e4e23e7d109e0cb596acc593b7cfbc61dfa
    Merge: 7452f2f 145b606
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 19:26:07 2024 +0200
    
        Merge branch 'main' into marcin
    
    commit 7452f2f7425f11d697f65ce0731509b096e514e8
    Author: Ostolski Marcin <ostolski.marcin@gmail.com>
    Date:   Tue May 21 19:20:15 2024 +0200
    
        gitignore
    
        gitignore

---
+++ app/Http/Controllers/RecipeIngredientController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\RecipeIngredient;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Routing\Controller;
+
+class RecipeIngredientController extends Controller
+{
+    public function index(): View
+    {
+        $models = RecipeIngredient::where('Is_Active', true)->get();
+        return view("RecipeIngredient.index", ["models" => $models]);
+    }
+
+    public function create(): View
+    {
+        $model = new RecipeIngredient();
+        $model->created_at = date('Y-m-d');
+        $model->updated_at = date('Y-m-d');
+        return view("RecipeIngredient.create", ["model" => $model]);
+    }
+    public function edit(int $id): View
+    {
+        $model = RecipeIngredient::find($id);
+        return view("RecipeIngredient.edit", ["model" => $model]);
+    }
+
+    public function addToDb(Request $request): RedirectResponse
+    {
+        $model = new RecipeIngredient();
+        $model->recipe_id = $request->input("Recipe_id");
+        $model->ingredient_id = $request->input("Ingredient_id");
+        $model->quantity = $request->input("Quantity");
+        $model->unit = $request->input("Unit");
+        $model->created_at = date('Y-m-d');
+        $model->updated_at = date('Y-m-d');
+        $model->isActive = true;
+        $model->save();
+        return redirect('/recipe-ingredient');
+    }
+    public function update(Request $request, int $id): RedirectResponse
+    {
+        $model = RecipeIngredient::find($id);
+        $model->recipe_id = $request->input("Recipe_id");
+        $model->ingredient_id = $request->input("Ingredient_id");
+        $model->quantity = $request->input("Quantity");
+        $model->unit = $request->input("Unit");
+        $model->updated_at = date('Y-m-d');
+        $model->save();
+        return redirect('/recipe-ingredient');
+    }
+
+    public function delete(Request $request, int $id): RedirectResponse
+    {
+        $model = RecipeIngredient::find($id);
+        $model->IsActive = false;
+        $model->save();
+        return redirect('/recipe-ingredient');
+    }
+}
