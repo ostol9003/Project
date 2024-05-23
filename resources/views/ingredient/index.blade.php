c2511e6e4ec4016f3d6d2c65bbc2e679cf8ddea8
Paulina Ostolska <pa.ostolska@gmail.com>
2024-05-23 13:37:37 +0200

    zmiana w wylladzie

--- resources/views/ingredient/index.blade.php
+++ resources/views/ingredient/index.blade.php
@@ -3,44 +3,35 @@
 @section('title', 'Ingredients')
 
 @section('menu')
-    <a class="btn btn-outline-secondary" href="/RecipeIngredient/create">Add new recipe ingredient</a>
-    <a class="btn btn-outline-secondary" href="/">Home</a>
+    <a class="btn btn-outline-secondary" href="/RecipeIngredient/create">New ingredient</a>
 @endsection
 
 @section('content')
 <div class="container mt-4">
     <div class="row gy-3">
         @foreach($models as $model)
-        <div class="col-sm-12 col-md-6 col-lg-4">
-            <div class="card">
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $model->id }}</h5>
-                    <p class="card-text">
-                        <strong>Recipe ID:</strong> {{ $model->recipe_id }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Ingredient ID:</strong> {{ $model->ingredient_id }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Quantity:</strong> {{ $model->quantity }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Unit:</strong> {{ $model->unit }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Status:</strong> {{ $model->is_active ? 'Active' : 'Inactive' }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Created At:</strong> {{ $model->created_at->format('d M Y') }}
-                    </p>
-                    <p class="card-text">
-                        <strong>Updated At:</strong> {{ $model->updated_at->format('d M Y') }}
-                    </p>
+                <h5 class="card-title">{{ $model->id }}</h5>
                 </div>
-                <div class="card-footer d-flex justify-content-between">
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>Recipe ID:</strong> {{ $model->recipe_id }}</li>
+                <li class="list-group-item"><strong>Ingredient ID:</strong> {{ $model->ingredient_id }}</li>
+                <li class="list-group-item"><strong>Quantity:</strong> {{ $model->quantity }}</li>
+                <li class="list-group-item"><strong>Unit:</strong> {{ $model->unit }}</li>
+                    <li class="list-group-item"><strong>Status:</strong> {{ $model->is_active ? 'Active' : 'Inactive' }}</li>
+                    <li class="list-group-item"><strong>Created At:</strong> {{ $model->created_at->format('d M Y') }}</li>
+                    <li class="list-group-item"><strong>Updated At:</strong> {{ $model->updated_at->format('d M Y') }}</li>
+                </ul>
+                <div class="card-body row">
+                    <div class="col">
                     <a href="/RecipeIngredient/{{ $model->id }}/edit" class="btn btn-outline-secondary">
                         <i class="bi bi-pencil"></i> Edit
                     </a>
+                    </div>
+                    <div class="col">
                     <form action="/RecipeIngredient/{{ $model->id }}" method="POST">
                         @csrf
                         @method('DELETE')
@@ -48,10 +39,13 @@
                             <i class="bi bi-trash"></i> Delete
                         </button>
                     </form>
+                    </div>
                 </div>
+
             </div>
         </div>
         @endforeach
     </div>
 </div>
+<br>
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,31 +1,75 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>@yield("title")</title>
-<link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link
-        href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
-        rel="stylesheet">
+    <title>@yield("title")</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ms+Madi&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>@yield("title")</h1>
+    <header class="header">
+        <h1 class="logo">
+            Cookiedough
+            <img src="{{ asset('images/chef-hat-svgrepo-com.svg') }}" alt="Logo">
+        </h1>
+    </header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid justify-content-center">
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('/images/chef-toque-svgrepo-com.svg') }}" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/categories">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/ingredients">Ingredients</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/recipes">Recipes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/recipe-category">Recipes-Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/recipe-ingredient">Recipes-Ingredients</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/users">Users</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+    <hr>
+    <div class="container-fluid">
+    <img src="/images/pineapple-supply-co-Q7PclNhVRI0-unsplash 1.png" style="height: 300px;" width="100%">
+    </div>
+    <br>
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-6">
+                <h2>@yield("title")</h2>
+            </div>
+            <div class="col-sm-6 text-end">
                 @yield("menu")
             </div>
         </div>
     </div>
+
     <hr>
-    <br />
-   @yield("content")
+    @yield("content")
     <script src="/js/bootstrap.min.js"></script>
 </body>
 
