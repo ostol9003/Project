9d64a262bc74f28c101a9b17b2c52b94a29ecdf7
Paulina Ostolska <pa.ostolska@gmail.com>
2024-05-26 00:15:23 +0200

    search

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,12 +11,12 @@
     <link rel="stylesheet" href="/css/style.css" />
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ms+Madi&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ms+Madi&display=swap" rel="stylesheet">
 </head>
 
 <body>
-<header class="header">
+    <header class="header">
         <h1 class="logo">
             Cookiedough
             <img src="{{ asset('images/chef-hat-svgrepo-com.svg') }}" alt="Logo">
@@ -24,9 +24,9 @@
     </header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid justify-content-center">
-        <a class="navbar-brand" href="/">
-        <img src="{{ asset('/images/chef-toque-svgrepo-com.svg') }}" alt="Logo">
-        </a>
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('/images/chef-toque-svgrepo-com.svg') }}" alt="Logo">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -52,30 +52,81 @@
                     </li>
                 </ul>
             </div>
+            <!-- Formularz wyszukiwania -->
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <form class="d-flex" action="/recipes/{{ request('name') }}" method="GET">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Search by name" name="name">
+                            <button class="btn btn-outline-secondary" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </nav>
         </div>
     </nav>
 
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/images/brooke-lark-jUPOXXRNdcA-unsplash.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/images/brooke-lark-jUPOXXRNdcA-unsplash.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/images/olayinka-babalola-r01ZopTiEV8-unsplash.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/images/rumman-amin-LNn6O_Mt730-unsplash.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-item">
-      <img src="/images/olayinka-babalola-r01ZopTiEV8-unsplash.jpg" class="d-block w-100" alt="...">
+
+    <div class="container mt-4">
+        <div class="row gy-3">
+            @foreach($models as $Recipe)
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $Recipe->title }}</h5>
+                        <p class="card-text">
+                            <strong>Cooking Time:</strong> {{ $Recipe->cooking_time }} minutes
+                        </p>
+                        <p class="card-text">
+                            <strong>Status:</strong> {{ $Recipe->is_active ? 'Active' : 'Inactive' }}
+                        </p>
+                        <p class="card-text">
+                            <strong>Created At:</strong> {{ $Recipe->created_at->format('d M Y') }}
+                        </p>
+                        <p class="card-text">
+                            <strong>Updated At:</strong> {{ $Recipe->updated_at->format('d M Y') }}
+                        </p>
+                        <p class="card-text">
+                            <strong>Description:</strong> {!! $Recipe->description !!}
+                        </p>
+                        <p class="card-text">
+                            <strong>Owner:</strong> {!! $Recipe->User->name !!} {!! $Recipe->User->email !!}
+                        </p>
+                        <p class="card-text">
+                            <strong>Ingredients:</strong>
+                            @foreach($Recipe->recipeIngredients as $recipeIngredient)
+                            {!! $recipeIngredient->ingredient->name !!} ({!! $recipeIngredient->ingredient->quantity !!} {!! $recipeIngredient->ingredient->unit !!})
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
     </div>
-    <div class="carousel-item">
-      <img src="/images/rumman-amin-LNn6O_Mt730-unsplash.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+
 
     <script src="/js/bootstrap.min.js"></script>
 </body>
