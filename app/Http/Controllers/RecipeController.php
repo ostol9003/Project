da35076f9500c3130b4988764f1e21fec62a1a29
Ostolski Marcin <ostolski.marcin@gmail.com>
2024-05-26 00:09:04 +0200

    Update RecipeController.php

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -16,7 +16,7 @@ use App\Models\User;
 class RecipeController extends Controller
 {
     public function index(Request $request): View
-    {        
+    {
         $name = $request->input('name');
 
         if ($name) {
@@ -38,20 +38,21 @@ class RecipeController extends Controller
         $users = User::all();
         $categories = Category::all();
         $ingredients = Ingredient::all();
-        return view('Recipe.create', [
+        return view('recipe.create', [
             'model' => $model,
             'users' => $users,
             'categories' => $categories,
             'ingredients' => $ingredients
         ]);
     }
+
     public function edit(int $id): View
-{
-    $model = Recipe::with('categories')->with('user')->find($id);
-    $users = User::all();
-    $categories = Category::all();
-    return view("recipe.edit", ["model" => $model,"users" => $users, "categories" => $categories]);
-}
+    {
+        $model = Recipe::with('categories')->with('user')->find($id);
+        $users = User::all();
+        $categories = Category::all();
+        return view("recipe.edit", ["model" => $model, "users" => $users, "categories" => $categories]);
+    }
 
 
     public function addToDb(Request $request): RedirectResponse
@@ -70,13 +71,13 @@ class RecipeController extends Controller
         $model->cooking_time = $request->input('cooking_time');
         $model->user_id = $request->input('user_id');
         $model->url = $request->input('url');
-        $model->is_promoted = $request->input('is_promoted')? true : false;
+        $model->is_promoted = $request->input('is_promoted') ? true : false;
         $model->created_at = date('Y-m-d');
         $model->updated_at = date('Y-m-d');
         $model->is_active = true;
         $model->save();
 
-   
+
         $categories = $request->input('categories', []);
         foreach ($categories as $categoryId) {
             $model->categories()->attach(
@@ -84,7 +85,7 @@ class RecipeController extends Controller
                 ['created_at' => now(), 'updated_at' => now()]
             );
 
-       
+
             if ($request->has('ingredients')) {
                 foreach ($request->input('ingredients') as $ingredientId => $details) {
                     if (isset($details['checked'])) {
@@ -113,14 +114,14 @@ class RecipeController extends Controller
             'ingredients.*.Quantity' => 'required_with:ingredients.*.checked|numeric|min:0.01',
             'ingredients.*.Unit' => 'required_with:ingredients.*.checked|string|max:5',
         ]);
-        
+
         $model = Recipe::find($id);
         $model->title = $request->input("title");
         $model->description = $request->input("description");
         $model->cooking_time = $request->input("cooking_time");
         $model->user_id = $request->input("user_id");
         $model->url = $request->input('url');
-        $model->is_promoted = $request->input('is_promoted')? true : false;
+        $model->is_promoted = $request->input('is_promoted') ? true : false;
         $model->updated_at = date('Y-m-d');
         $model->save();
         return redirect('/recipes');
@@ -129,20 +130,17 @@ class RecipeController extends Controller
     public function delete(Request $request, int $id): RedirectResponse
     {
         $model = Recipe::find($id);
-    
+
         if ($model) {
             $model->is_active = false;
             $model->save();
-    
-            RecipeCategory::
-                where('recipe_id', $model->id)
+
+            RecipeCategory::where('recipe_id', $model->id)
                 ->update(['is_active' => false, 'updated_at' => now()]);
 
             $model->recipeIngredients()->update(['is_active' => false, 'updated_at' => now()]);
         }
-    
+
         return redirect('/recipes');
     }
-    
-    
 }
