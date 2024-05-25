c0130cf36790ad7b941afc2a1fa1269b1030ba4f
Paulina Ostolska <pa.ostolska@gmail.com>
2024-05-25 22:33:41 +0200

    ingredient create

--- resources/views/ingredient/edit.blade.php
+++ resources/views/ingredient/edit.blade.php
@@ -6,14 +6,13 @@
 
 @section('content')
 <div class="container mt-5">
-    <h1>New ingredient</h1>
+    <h1>Edit ingredient</h1>
     <form method="POST" action="/ingredients/add-to-db">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control" name="name" value="{{$model->name}}" required>
         </div>
-        
         <button type="submit" class="btn btn-outline-secondary" type="submit">Create</button>
     </form>
 </div>
